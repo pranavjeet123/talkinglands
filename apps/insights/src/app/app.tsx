ea264fa664e7c12e058b4e76b3e07681f42a9a4e
Pranavjeet <pranavjeetmishra@gmail.com>
2025-11-16 17:32:31 +0530

    Added MFE shell , Remotes as map and insights
    Added NX monorepo with 2 remote apps built on react , tailwind css configured

---
+++ apps/insights/src/app/app.tsx
@@ -0,0 +1,12 @@
+import NxWelcome from './nx-welcome';
+import '../styles.css';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="insights" />
+    </div>
+  );
+}
+
+export default App;
